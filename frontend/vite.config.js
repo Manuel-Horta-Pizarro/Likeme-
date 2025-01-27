4570e4f3a7ffbe59d50ac086fee063e8accc5456
manuel-horta-pizarro <manuel.horta.pizarro@gmail.com>
2025-01-27 17:25:04 -0300

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    port: 3000, 
+    port: 5173, 
+    proxy: {
+      "/posts": "http://localhost:3000", 
+    },
   },
 });
